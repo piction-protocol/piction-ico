34af636fa0d0dcdcff57c3671c120be794925056
jhSeo <jh.seo@battleent.com>
2018-06-20 17:20:13 +0900

    Lock 상태를 가져오는 Method 수정

--- contracts/token/PXL.sol
+++ contracts/token/PXL.sol
@@ -51,7 +51,7 @@ contract PXL is StandardToken, ExtendsOwnable {
         isTransferable = true;
     }
 
-    function getTokenTransferable() external onlyOwner view returns (bool) {
+    function getTokenTransferable() public view returns (bool) {
         return isTransferable;
     }
 
