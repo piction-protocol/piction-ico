3aa5faa43bf970ff48cedf8b8c3acb081add1fc3
charls.kim <cs.kim@battleent.com>
2018-06-25 18:45:09 +0900

    Sale contract 스펙변경 - 구매, 환불, 주소 변경

--- contracts/sale/Sale.sol
+++ contracts/sale/Sale.sol
@@ -121,29 +121,23 @@ contract Sale is Stateable {
         require(whiteList.whitelist(buyer));
         require(buyer != address(0));
         require(weiRaised[productAddress] < product.maxcap());
-
-        uint256 buyerAmount = tokenDistributor.getEtherAmount(buyers[productAddress][buyer]);
-
-        require(buyerAmount < product.exceed());
-        require(buyerAmount.add(amount) >= product.minimum());
+        require(buyers[productAddress][buyer] < product.exceed());
+        require(buyers[productAddress][buyer].add(amount) >= product.minimum());
 
         uint256 purchase;
         uint256 refund;
-        (purchase, refund) = getPurchaseDetail(buyerAmount, amount, productAddress);
+        (purchase, refund) = getPurchaseDetail(buyers[productAddress][buyer], amount, productAddress);
 
-        if(buyerAmount > 0) {
-            tokenDistributor.addPurchased(buyers[productAddress][buyer], purchase.mul(product.rate()), purchase);
-        } else {
-            buyers[productAddress][buyer] = tokenDistributor.setPurchased(buyer, productAddress, purchase.mul(product.rate()), purchase);
+        if(purchase > 0) {
+            wallet.transfer(purchase);
+            tokenDistributor.addPurchased(buyer, productAddress, purchase.mul(product.rate()), purchase);
+            weiRaised[productAddress] = weiRaised[productAddress].add(purchase);
+            buyers[productAddress][buyer] = buyers[productAddress][buyer].add(purchase);
         }
-
-        wallet.transfer(purchase);
 
         if(refund > 0) {
             buyer.transfer(refund);
         }
-
-        weiRaised[productAddress] = weiRaised[productAddress].add(purchase);
 
         if(weiRaised[productAddress] >= product.maxcap()) {
             setState(State.Finished);
@@ -152,13 +146,13 @@ contract Sale is Stateable {
         emit Purchase(buyer, purchase, refund, purchase.mul(product.rate()));
     }
 
-    function getPurchaseDetail(uint256 _buyerAmount, uint256 _amount, address _product)
+    function getPurchaseDetail(uint256 _raisedAmount, uint256 _amount, address _product)
         private
         view
         returns (uint256, uint256)
     {
         uint256 d1 = product.maxcap().sub(weiRaised[_product]);
-        uint256 d2 = product.exceed().sub(_buyerAmount);
+        uint256 d2 = product.exceed().sub(_raisedAmount);
         uint256 possibleAmount = (d1.min256(d2)).min256(_amount);
 
         return (possibleAmount, _amount.sub(possibleAmount));
@@ -173,50 +167,57 @@ contract Sale is Stateable {
         return weiRaised[_product];
     }
 
-    function refund(address _product, address _buyer)
+    function refund(uint256 _id, address _product, address _buyer)
         external
-        onlyOwner
+        payable
         validAddress(_product)
         validAddress(_buyer)
     {
-        require(buyers[_product][_buyer] > 0);
+        require(msg.sender == wallet);
+        require(_id > 0);
 
         bool isRefund;
         uint256 refundAmount;
-        (isRefund, refundAmount) = tokenDistributor.refund(buyers[_product][_buyer]);
+        (isRefund, refundAmount) = tokenDistributor.refund(_id, _product, _buyer);
 
-        require(refundAmount > 0);
+        require(isRefund && refundAmount > 0);
 
-        if(isRefund) {
+        _buyer.transfer(refundAmount);
+
+        if(address(product) == _product
+            && (getState() == State.Starting || getState() == State.Pausing))
+        {
             weiRaised[_product] = weiRaised[_product].sub(refundAmount);
-            delete buyers[_product][_buyer];
+            buyers[_product][_buyer] = buyers[_product][_buyer].sub(refundAmount);
         }
     }
 
-    function buyerAddressTransfer(address _product, address _from, address _to)
+    function buyerAddressTransfer(uint _id, address _product, address _from, address _to)
         external
         onlyOwner
         validAddress(_product)
         validAddress(_from)
         validAddress(_to)
     {
+        require(_id > 0);
         require(whiteList.whitelist(_from));
         require(whiteList.whitelist(_to));
-        require(tokenDistributor.getEtherAmount(buyers[_product][_from]) > 0);
-        require(tokenDistributor.getEtherAmount(buyers[_product][_to]) == 0);
+        require(buyers[_product][_from] > 0);
+        require(buyers[_product][_to] == 0);
 
-        bool isChanged = tokenDistributor.buyerAddressTransfer(buyers[_product][_from], _from, _to);
+        bool isChanged;
+        uint256 transferAmount;
+        (isChanged, transferAmount) = tokenDistributor.buyerAddressTransfer(_id, _from, _to);
 
-        require(isChanged);
+        require(isChanged && transferAmount > 0);
 
-        uint256 fromId = buyers[_product][_from];
-        buyers[_product][_to] = fromId;
-        delete buyers[_product][_from];
+        buyers[_product][_from] = buyers[_product][_from].sub(transferAmount);
+        buyers[_product][_to] = buyers[_product][_to].add(transferAmount);
 
-        emit BuyerAddressTransfer(_from, _to, buyers[_product][_to]);
+        emit BuyerAddressTransfer(_from, _to, transferAmount);
     }
 
     event Purchase(address indexed _buyer, uint256 _purchased, uint256 _refund, uint256 _tokens);
     event ChangeExternalAddress(address _addr, string _name);
-    event BuyerAddressTransfer(address indexed _from, address indexed _to, uint256 _id);
+    event BuyerAddressTransfer(address indexed _from, address indexed _to, uint256 _amount);
 }
