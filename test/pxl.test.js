146dfe3fe2636fbefb0d17a23b590871f22b1012
charls.kim <charls.kim.dev@gmail.com>
2018-12-19 15:25:04 +0900

    PXL 컨트랙트 최신 코드 반영하여 테스트 코드 작성

--- test/pxl.test.js
+++ test/pxl.test.js
@@ -1,9 +1,6 @@
-const { timer } = require("./helpers/timer");
-
 var Pxl = artifacts.require("PXL");
 
 const colors = require('colors/safe');
-
 const BigNumber = web3.BigNumber;
 
 require("chai")
@@ -12,48 +9,48 @@ require("chai")
     .should();
 
 contract('PXL', function(accounts) {
-    const owner = accounts[0];
-    const notOwner = accounts[1];
-    const userOne = accounts[2];
-    const userTwo = accounts[3];
+    const newOwner = accounts[1];
+    const deployOwner = accounts[2];
+    const addOwner = accounts[3];
+    const user1 = accounts[4];
 
     const decimals = Math.pow(10, 18);
     const initialBalance = 10000 * decimals;
     const sendPXL = 3000 * decimals;
-    const userSendPXL = 100 * decimals;
+    const ownerPXL = 100 * decimals;
 
     let token;
 
     let beforeBalance;
     let afterBalance;
     let txFee;
-    let accumulate = 0;
-    let won = 450000;
+    let won = 150000;
+    let fastGwei = 15000000000;
 
     it("setup contract ", async () => {
-        console.log(colors.red.bold("\t++++++++ 1ETH to 450,000 won ++++++++"));
+        console.log(colors.red.bold("\t++++++++ 1ETH to 150,000 won ++++++++"));
 
         console.log();
-        console.log(colors.magenta.bold("\t========== Deploy contract gas usage(1 Gwei) =========="));
+        console.log(colors.magenta.bold("\t========== Deploy contract gas usage(15 Gwei) =========="));
 
-        beforeBalance = await web3.eth.getBalance(notOwner);
+        beforeBalance = await web3.eth.getBalance(deployOwner);
 
-        token = await Pxl.new({from: notOwner, gasPrice: 1000000000}).should.be.fulfilled;
+        token = await Pxl.new({from: deployOwner, gasPrice: fastGwei}).should.be.fulfilled;
 
-        afterBalance = await web3.eth.getBalance(notOwner);
+        afterBalance = await web3.eth.getBalance(deployOwner);
         txFee = beforeBalance - afterBalance;
         console.log(colors.magenta("\ttoken contract"));
         console.log(colors.magenta("\tActual Tx Cost/Fee : " + txFee / decimals + " Ether (￦ " + Math.round((txFee / decimals) * won) + ")"));
         console.log();
 
         console.log();
-        console.log(colors.magenta.bold("\t========== TransferOwnership gas usage(1 Gwei) =========="));
+        console.log(colors.magenta.bold("\t========== TransferOwnership gas usage(15 Gwei) =========="));
 
-        beforeBalance = await web3.eth.getBalance(notOwner);
+        beforeBalance = await web3.eth.getBalance(deployOwner);
 
-        await token.transferOwnership(owner, {from: notOwner, gasPrice: 1000000000}).should.be.fulfilled;
+        await token.transferOwnership(newOwner, {from: deployOwner, gasPrice: fastGwei}).should.be.fulfilled;
 
-        afterBalance = await web3.eth.getBalance(notOwner);
+        afterBalance = await web3.eth.getBalance(deployOwner);
         txFee = beforeBalance - afterBalance;
         console.log(colors.magenta("\ttransferOwnership"));
         console.log(colors.magenta("\tActual Tx Cost/Fee : " + txFee / decimals + " Ether (￦ " + Math.round((txFee / decimals) * won) + ")"));
@@ -61,13 +58,13 @@ contract('PXL', function(accounts) {
 
 
         console.log();
-        console.log(colors.magenta.bold("\t========== Mint gas usage(1 Gwei) =========="));
+        console.log(colors.magenta.bold("\t========== Mint gas usage(15 Gwei) =========="));
 
-        beforeBalance = await web3.eth.getBalance(owner);
+        beforeBalance = await web3.eth.getBalance(newOwner);
 
-        await token.mint(initialBalance, {from: owner, gasPrice: 1000000000}).should.be.fulfilled;
+        await token.mint(initialBalance, {from: newOwner, gasPrice: fastGwei}).should.be.fulfilled;
 
-        afterBalance = await web3.eth.getBalance(owner);
+        afterBalance = await web3.eth.getBalance(newOwner);
         txFee = beforeBalance - afterBalance;
         console.log(colors.magenta("\ttoken mint"));
         console.log(colors.magenta("\tActual Tx Cost/Fee : " + txFee / decimals + " Ether (￦ " + Math.round((txFee / decimals) * won) + ")"));
@@ -75,19 +72,19 @@ contract('PXL', function(accounts) {
     });
 
     it ("check initial balance", async () => {
-        const balance = await token.balanceOf.call(owner);
+        const balance = await token.balanceOf.call(newOwner);
         balance.should.be.bignumber.equal(initialBalance);
     });
 
-    it ("send 3000 pixel to userOne", async () => {
+    it ("send 3000 pixel to user1", async () => {
         console.log();
-        console.log(colors.magenta.bold("\t========== Token transfer gas usage(1 Gwei) =========="));
+        console.log(colors.magenta.bold("\t========== Token transfer gas usage(15 Gwei) =========="));
 
-        beforeBalance = await web3.eth.getBalance(owner);
+        beforeBalance = await web3.eth.getBalance(newOwner);
 
-        await token.transfer(userOne, sendPXL, {from: owner, gasPrice: 1000000000}).should.be.fulfilled;
+        await token.transfer(user1, sendPXL, {from: newOwner, gasPrice: fastGwei}).should.be.fulfilled;
 
-        afterBalance = await web3.eth.getBalance(owner);
+        afterBalance = await web3.eth.getBalance(newOwner);
         txFee = beforeBalance - afterBalance;
         console.log(colors.magenta("\tpxl transfer 3000"));
         console.log(colors.magenta("\tActual Tx Cost/Fee : " + txFee / decimals + " Ether (￦ " + Math.round((txFee / decimals) * won) + ")"));
@@ -95,37 +92,52 @@ contract('PXL', function(accounts) {
     });
 
     it ("check token transfer", async () => {
-        const ownerBalance = await token.balanceOf.call(owner);
+        const ownerBalance = await token.balanceOf.call(newOwner);
         ownerBalance.should.be.bignumber.equal(initialBalance - sendPXL);
 
-        const userOneBalance = await token.balanceOf.call(userOne);
-        userOneBalance.should.be.bignumber.equal(sendPXL);
+        const user1Balance = await token.balanceOf.call(user1);
+        user1Balance.should.be.bignumber.equal(sendPXL);
     });
 
-    it ("notOwner token transfer", async () => {
-        await token.transfer(notOwner, sendPXL, {from: owner, gasPrice: 1000000000}).should.be.fulfilled;
-        await token.transfer(userOne, sendPXL, {from: notOwner, gasPrice: 1000000000}).should.be.rejected;
+    it ("deployOwner token transfer", async () => {
+        await token.transfer(newOwner, sendPXL, {from: deployOwner, gasPrice: fastGwei}).should.be.rejected;
     });
 
-    it ("unlock", async () => {
-        await token.approve(userTwo, userSendPXL, {from: userOne, gasPrice: 1000000000}).should.be.fulfilled;
-        await token.transferFrom(userOne, userTwo, userSendPXL, {from: userTwo, gasPrice: 1000000000}).should.be.rejected;
-        await token.transfer(userTwo, userSendPXL, {from: userOne, gasPrice: 1000000000}).should.be.rejected;
+    it ("add contract owner", async() => {
+        await token.addOwner(addOwner, {from: newOwner, gasPrice: fastGwei}).should.be.fulfilled;
 
-        console.log();
-        console.log(colors.magenta.bold("\t========== unlock gas usage(1 Gwei) =========="));
+        const isOwner = await token.isOwner.call(addOwner);
+        isOwner.should.be.equal(true);
 
-        beforeBalance = await web3.eth.getBalance(owner);
+        await token.transfer(addOwner, ownerPXL, {from: newOwner, gasPrice: fastGwei}).should.be.fulfilled;
 
-        await token.unlock({from: owner, gasPrice: 1000000000}).should.be.fulfilled;
+        let addOwnerBalance = await token.balanceOf.call(addOwner);
+        addOwnerBalance.should.be.bignumber.equal(ownerPXL);
 
-        afterBalance = await web3.eth.getBalance(owner);
-        txFee = beforeBalance - afterBalance;
-        console.log(colors.magenta("\tunlock"));
-        console.log(colors.magenta("\tActual Tx Cost/Fee : " + txFee / decimals + " Ether (￦ " + Math.round((txFee / decimals) * won) + ")"));
-        console.log();
+        await token.transfer(newOwner, ownerPXL/2, {from: addOwner, gasPrice: fastGwei}).should.be.fulfilled;
+        addOwnerBalance = await token.balanceOf.call(addOwner);
+        addOwnerBalance.should.be.bignumber.equal(ownerPXL/2);
 
-        await token.transfer(userTwo, userSendPXL, {from: userOne, gasPrice: 1000000000}).should.be.fulfilled;
-        await token.transferFrom(userOne, userTwo, userSendPXL, {from: userTwo, gasPrice: 1000000000}).should.be.fulfilled;
+        await token.mint(ownerPXL/2, {from: addOwner, gasPrice: fastGwei}).should.be.fulfilled;
+        addOwnerBalance = await token.balanceOf.call(addOwner);
+        addOwnerBalance.should.be.bignumber.equal(ownerPXL);
+    });
+
+    it ("remove contract owner", async() => {
+        await token.removeOwner(addOwner, {from: newOwner, gasPrice: fastGwei}).should.be.fulfilled;
+        
+        const isOwner = await token.isOwner.call(addOwner);
+        isOwner.should.be.equal(false);
+
+        let addOwnerBalance = await token.balanceOf.call(addOwner);
+        addOwnerBalance.should.be.bignumber.equal(ownerPXL);
+
+        await token.transfer(newOwner, ownerPXL, {from: addOwner, gasPrice: fastGwei}).should.be.fulfilled;
+        addOwnerBalance = await token.balanceOf.call(addOwner);
+        addOwnerBalance.should.be.bignumber.equal(0);
+
+        await token.mint(ownerPXL, {from: addOwner, gasPrice: fastGwei}).should.be.rejected;
+        addOwnerBalance = await token.balanceOf.call(addOwner);
+        addOwnerBalance.should.be.bignumber.equal(0);
     });
 });
