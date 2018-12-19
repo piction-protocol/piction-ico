278ca9f45000e29ce8ca0bc69fe456bf85007a67
Jiwoong Heo <jiwoong.heo@gmail.com>
2018-12-19 10:21:31 +0900

    Can remove owner

--- contracts/utils/ExtendsOwnable.sol
+++ contracts/utils/ExtendsOwnable.sol
@@ -5,6 +5,7 @@ contract ExtendsOwnable {
     mapping(address => bool) public owners;
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event OwnershipRevoked(address indexed revokedOwner);
     event OwnershipExtended(address indexed host, address indexed guest);
 
     modifier onlyOwner() {
@@ -20,6 +21,13 @@ contract ExtendsOwnable {
         require(guest != address(0));
         owners[guest] = true;
         emit OwnershipExtended(msg.sender, guest);
+    }
+
+    function removeOwner(address owner) public onlyOwner {
+        require(owner != address(0));
+        require(msg.sender != owner);
+        owners[owner] = false;
+        emit OwnershipRevoked(owner);
     }
 
     function transferOwnership(address newOwner) public onlyOwner {
